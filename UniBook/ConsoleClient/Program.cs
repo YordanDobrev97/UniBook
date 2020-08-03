98f0b578caf991710adf1b6f8a5600ff58d4f718
Yordan Dobrev <42092212+YordanDobrev97@users.noreply.github.com>
2020-08-03 23:12:51 +0300

    Seed data in database

--- UniBook/ConsoleClient/Program.cs
+++ UniBook/ConsoleClient/Program.cs
@@ -10,21 +10,21 @@ namespace ConsoleClient
         static void Main()
         {
             var db = new UniBookDbContext();
-            //var book = File.ReadAllText("../../../PetdesetNuansaPoTumno.txt");
+            var book = File.ReadAllText("../../../Keri.txt");
 
-            //db.Books.Add(new Book
-            //{
-            //    Title = "Петдесет нюанса по-тъмно",
-            //    Author = new Author
-            //    {
-            //        Name = "Е. Л. Джеймс"
-            //    },
-            //    ImgUrl = "https://i.ytimg.com/vi/wrZFTAbWuaw/maxresdefault.jpg",
-            //    Rating = 0,
-            //    Content = book
-            //});
+            db.Books.Add(new Book
+            {
+                Title = "Кери",
+                Author = new Author
+                {
+                    Name = "Стивън Кинг"
+                },
+                ImgUrl = "https://cdn.ozone.bg/media/catalog/product/cache/1/image/a4e40ebdc3e371adff845072e1c73f37/k/e/bfda8cd69bf5273ae8371e3e7f60ddd6/keri-31.jpg",
+                Rating = 0,
+                Content = book
+            });
 
-            //db.SaveChanges();
+            db.SaveChanges();
         }
     }
 }
