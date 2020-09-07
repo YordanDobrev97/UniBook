b5b78dafc3d57b4dc9bd3d2a45e19953745aa961
Yordan Dobrev <42092212+YordanDobrev97@users.noreply.github.com>
2020-09-07 20:09:05 +0300

    ForumController and ReviewController files

---
+++ UniBook/UniBook.Web/Controllers/ForumController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UniBook.Web.Controllers
+{
+    public class ForumController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
---
+++ UniBook/UniBook.Web/Controllers/ReviewController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UniBook.Web.Controllers
+{
+    public class ReviewController : Controller
+    {
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
