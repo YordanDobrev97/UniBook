0bd0fc223cb7cc75deab9760bb3056db0d11faa7
Yordan Dobrev <42092212+YordanDobrev97@users.noreply.github.com>
2020-10-08 02:55:50 +0300

    Description of Forum Models

---
+++ Data/UniBook.Data.Models/Post.cs
@@ -0,0 +1,23 @@
+﻿namespace UniBook.Data.Models
+{
+    using System.Collections.Generic;
+
+    public class Post
+    {
+        public Post()
+        {
+            this.Tags = new HashSet<Tag>();
+            this.PostComments = new HashSet<PostComment>();
+        }
+
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public int Votes { get; set; }
+
+        public ICollection<Tag> Tags { get; set; }
+
+        public ICollection<PostComment> PostComments { get; set; }
+    }
+}
---
+++ Data/UniBook.Data.Models/PostComment.cs
@@ -0,0 +1,11 @@
+﻿namespace UniBook.Data.Models
+{
+    public class PostComment
+    {
+        public int Id { get; set; }
+
+        public int PostId { get; set; }
+
+        public string CommentBody { get; set; }
+    }
+}
