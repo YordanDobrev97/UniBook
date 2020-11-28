bd7ecc10450feba8ff17debb9e6a1eff8c158401
Yordan Dobrev <42092212+YordanDobrev97@users.noreply.github.com>
2020-11-28 04:09:54 +0200

    Add Profile page

---
+++ Web/UniBook.Web/Controllers/ProfileController.cs
@@ -0,0 +1,12 @@
+﻿namespace UniBook.Web.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class ProfileController : BaseController
+    {
+        public IActionResult Details()
+        {
+            return this.View();
+        }
+    }
+}
